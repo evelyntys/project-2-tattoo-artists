ca5c670a1f7a0df56155a0c156727ac68eea0966
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-07 11:14:50 +0000

    added skeleton for form input

--- tattoo-artists/src/App.js
+++ tattoo-artists/src/App.js
@@ -22,116 +22,149 @@ class App extends React.Component {
           </Container>
         </Navbar>
 
-        <Container>
-          <Form>
-            <Container>
-              <Form.Label>Name: </Form.Label>
-              <Form.Control type="text" placeholder="enter your name" />
-            </Container>
+        <div className="container">
+          <div className="container">
+            <h1>Information about the artist</h1>
+            <div>
+              <label className="form-label">Name of tattoo artist: </label>
+              <input type="text" className="form-control" placeholder="artist name" name="name" />
+            </div>
 
-            <Container>
-              <Form.Label>Gender: </Form.Label>
-              <div className="mb-3">
-                <Form.Check
-                  inline
-                  label="Female"
-                  name="gender"
-                  type="radio"
-                  value="female"
-                  id="female"
-                />
-                <Form.Check
-                  inline
-                  label="Male"
-                  name="gender"
-                  type="radio"
-                  id="male"
-                  value="male"
-                />
-                <Form.Check
-                  inline
-                  label="Others"
-                  name="gender"
-                  type="radio"
-                  id="others"
-                  value="others"
-                  checked
-                />
-              </div>
-            </Container>
+            <div>
+              <label className="form-label">Gender: </label>
+              <input type="radio" className="form-check-input mx-2" value="female" name="gender" /><label className="form-check-label">Female</label>
+              <input type="radio" className="form-check-input mx-2" value="male" name="gender" /><label className="form-check-label">Male</label>
+              <input type="radio" className="form-check-input mx-2" value="others" name="gender" checked /><label className="form-check-label">Others</label>
+            </div>
 
-            <Container>
-              <Form.Label>Year started: </Form.Label>
-              <Form.Control type="text" placeholder="enter the year you started tattooing" />
-            </Container>
+            <div>
+              <label className="form-label">Year started tattooing: </label>
+              <input type="text" className="form-control" placeholder="year you started tattooing" name="name" />
+            </div>
 
-            <Container>
-              <Form.Label>Are you a tattoo apprentice? </Form.Label>
-              <div className="mb-3">
-                <Form.Check
-                  inline
-                  label="Yes"
-                  name="apprentice"
-                  type="radio"
-                  value="yes"
-                  id="yes"
-                />
-                <Form.Check
-                  inline
-                  label="No"
-                  name="apprentice"
-                  type="radio"
-                  id="no"
-                  value="no"
-                  checked
-                />
-              </div>
-            </Container>
+            <div>
+              <label className="form-label">Are you an apprentice? </label>
+              <input type="radio" className="form-check-input mx-2" value="yes" name="apprentice" /><label className="form-check-label">Yes</label>
+              <input type="radio" className="form-check-input mx-2" value="no" name="apprentice" checked /><label className="form-check-label">No</label>
+            </div>
 
-            <Container>
-              <Form.Label>Please select your methods of tattooing: </Form.Label>
-              <div className="mb-3">
-                <Form.Check
-                  inline
-                  label="Handpoke"
-                  name="methods"
-                  type="checkbox"
-                  value="handpoke"
-                  id="handpoke"
-                />
-                <Form.Check
-                  inline
-                  label="Machine"
-                  name="methods"
-                  type="checkbox"
-                  id="machine"
-                  value="machine"
-                />
-                <Form.Check
-                  inline
-                  label="Jagua"
-                  name="methods"
-                  type="checkbox"
-                  id="jagua"
-                  value="jagua"
-                />
-              </div>
-            </Container>
+            <div>
+              <label className="form-label">Please select your method(s) of tattooing:</label>
+              <input type="checkbox" className="form-check-input mx-2" value="handpoke" name="methods" /><label className="form-check-label">Handpoke</label>
+              <input type="checkbox" className="form-check-input mx-2" value="machine" name="methods" /><label className="form-check-label">Machine</label>
+              <input type="checkbox" className="form-check-input mx-2" value="jagua" name="methods" /><label className="form-check-label">Jagua</label>
+            </div>
 
-            <Container>
-              <Form.Label>Is it temporary?</Form.Label>
-              <Form.Select aria-label="Default select example" name="temporary">
+            <div>
+              <label className="form-label">Is it temporary? </label>
+              <select class="form-select" aria-label="Default select example">
+                <option selected>Please select...</option>
                 <option value="no">No</option>
                 <option value="yes">Yes</option>
-              </Form.Select>
-            </Container>
+              </select>
+            </div>
 
-          </Form>
+            <div>
+              <label className="form-label">Please select your style(s) of tattoo (up to 3): </label>
+              {/* to put multiselect */}
+              <select class="form-select" aria-label="Default select example" multiple>
+                <option value="surrealism">Surrealism</option>
+                <option value="traditional-americana">Traditional Americana</option>
+                <option value="traditional-japanese">Traditional Japanese</option>
+                <option value="blackwork">Blackwork</option>
+                <option value="minimalist">Minimalist</option>
+                <option value="water-colour">Water Colour</option>
+                <option value="pet/animals">Pet/Animals</option>
+                <option value="floral">Floral</option>
+              </select>
+            </div>
 
-        </Container>
+
+            <div>
+              <label className="form-label">Please select your ink(s):</label>
+              <input type="checkbox" className="form-check-input mx-2" value="black" name="methods" /><label className="form-check-label">Black</label>
+              <input type="checkbox" className="form-check-input mx-2" value="colours" name="methods" /><label className="form-check-label">Colours</label>
+              <input type="checkbox" className="form-check-input mx-2" value="jagua" name="methods" /><label className="form-check-label">Jagua</label>
+              <input type="checkbox" className="form-check-input mx-2" value="uv" name="methods" /><label className="form-check-label">UV</label>
+            </div>
+
+            <div>
+              <label className="form-label">Please enter your contact details: </label>
+              <div className="row">
+                <div className="col">
+                  <input type="text" className="form-control" name="contactKey" placeholder="platform e.g. instagram" />
+                </div>
+                <div className="col">
+                  <input type="text" className="form-control" name="contactValue" placeholder="your contact" />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label className="form-label">Please provide links to your reference artwork (up to 3): </label>
+              <input type="text" className="form-control" placeholder="image link" name="images" />
+              <input type="text" className="form-control" placeholder="image link" name="images" />
+              <input type="text" className="form-control" placeholder="image link" name="images" />
+            </div>
+
+            <div>
+              <label className="form-label">Your name: </label>
+              <input type="text" className="form-control" placeholder="e.g. John Doe" name="ownerName" />
+              <label className="form-label">Your email: </label>
+              <input type="text" className="form-control" placeholder="e.g. johndoe@email.com" name="ownerEmail" />
+            </div>
+          </div>
+
+          <div className="container">
+            <h1>Information about the studio</h1>
+            <div>
+              <label className="form-label">Name of studio: </label>
+              <input type="text" className="form-control" placeholder="studio name" name="studio-name" />
+            </div>
+
+            <div>
+              <label className="form-label">Is it a private studio? </label>
+              <input type="radio" className="form-check-input mx-2" value="yes" name="private" /><label className="form-check-label">Yes</label>
+              <input type="radio" className="form-check-input mx-2" value="no" name="private" checked /><label className="form-check-label">No</label>
+            </div>
+
+            <div>
+              <label className="form-label">Are bookings required? </label>
+              <input type="radio" className="form-check-input mx-2" value="yes" name="bookingsRequired" /><label className="form-check-label">Yes</label>
+              <input type="radio" className="form-check-input mx-2" value="no" name="bookingsRequired" checked /><label className="form-check-label">No</label>
+            </div>
+
+            <div>
+              <h6>Address</h6>
+              <label className="form-label">Street: (please enter "nil" if not applicable)</label>
+              <input type="text" className="form-control" placeholder="street" name="street" />
+              <label className="form-label">Unit: (please enter "nil" if not applicable)</label>
+              <input type="text" className="form-control" placeholder="unit" name="unit" />
+              <label className="form-label">Postal Code: (please enter "000000" if not applicable)</label>
+              <input type="text" className="form-control" placeholder="postal code" name="postal" />
+            </div>
+
+            <div>
+              <label className="form-label">Does your studio offer any other services? (please enter nil if no): </label>
+              <input type="text" className="form-control" placeholder="e.g. piercings" name="otherServices" />
+            </div>
+
+            <button className="btn btn-primary mt-2">Add new artist</button>
+          </div>
+
+          
+        </div>
       </React.Fragment>
     );
   }
 }
+
+
+
+
+// otherServices
+// :
+// Array
+
 
 export default App;
