729ad98b84511e195751f6b47830343ace073561
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-09 15:14:05 +0000

    integrated delete w/ express, to debug validation

--- tattoo-artists/src/App.js
+++ tattoo-artists/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Navbar, NavDropdown, Container, Form } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Container} from 'react-bootstrap';
 import './App.css';
 import AddNewArtist from './AddNewArtist';
+import ShowAllArtists from './ShowAllArtists';
 
 class App extends React.Component {
+  state = {
+    active: "home"
+  }
+
+  changeActive(page) {
+    this.setState({
+      active: page
+    })
+  }
+
+  changePages(){
+    if (this.state.active==="add-new"){
+      return <AddNewArtist />
+    }
+    else if (this.state.active==="view-all"){
+      return <ShowAllArtists/>
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -14,10 +34,10 @@ class App extends React.Component {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link className="mx-auto" href="#about-us">About Us / FAQ</Nav.Link>
-                <Nav.Link className="mx-auto" href="#view-all">View all</Nav.Link>
-                <Nav.Link className="mx-auto" href="#explore">Explore</Nav.Link>
-                <Nav.Link className="mx-auto" href="#add-new">Add new</Nav.Link>
+                <Nav.Link className="mx-auto" value ='about-us' onClick={() => {this.changeActive("about-us")}} >About Us / FAQ</Nav.Link>
+                <Nav.Link className="mx-auto" value='view-all'  onClick={() => {this.changeActive("view-all")}} >View all</Nav.Link>
+                <Nav.Link className="mx-auto" value='explore'  onClick={() => {this.changeActive("explore")}}>Explore</Nav.Link>
+                <Nav.Link className="mx-auto" value='add-new'  onClick={() => {this.changeActive("add-new")}} >Add new</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
@@ -27,20 +47,16 @@ class App extends React.Component {
           <div height="100%" width="100%">
             <img width="100%" src="https://drive.google.com/uc?id=12PQFEtg6EJ7kSUiO8MWeHwYQ0O74b8hd" style={{ "objectFit": "cover" }} />
           </div>
+          <div height="100%" width="100%" style={{"backgroundColor": "black"}}>
+
+          </div>
         </div>
 
-        <AddNewArtist />
+        {this.changePages()}
       </React.Fragment>
     );
   }
 }
-
-
-
-
-// otherServices
-// :
-// Array
 
 
 export default App;
--- tattoo-artists/src/ShowAllArtists.js
+++ tattoo-artists/src/ShowAllArtists.js
@@ -1,27 +1,118 @@
 import React from 'react';
 import axios from 'axios';
+import { Button, Modal } from 'react-bootstrap';
 
-export default class ShowAllArtists extends React.Component{
+export default class ShowAllArtists extends React.Component {
     url = "https://8888-evelyntys-project2restf-q3ufqgdmigx.ws-us53.gitpod.io/"
     state = {
-        data: []
+        data: [],
+        show: false,
+        email: ""
     }
-    async componentDidMount(){
+    async componentDidMount() {
         let response = await axios.get(this.url + 'show-artists');
         this.setState({
             data: response.data
         })
         console.log(response.data)
-        console.log(this.state.data)
     }
+
+    updateFormField = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    processDelete = async (id) => {
+        try {
+            let response = await axios.delete(this.url + `tattoo-artist/${id}`, {
+            'email': this.state.email
+        })
+        console.log(response.data)
+    }
+        catch(e){
+            alert(404)
+            console.log(e)
+            console.log(id)
+            console.log(this.url + `tattoo-artist/${id}`)
+            console.log(this.state.email)
+        }
+        
+    }
+
+
+    ConfirmDelete(artist) {
+
+        const handleClose = () => this.setState({
+            show: false
+        });
+        const handleShow = () => this.setState({
+            show: true
+        });
+
+        return (
+            <React.Fragment>
+                <Button variant="danger" onClick={handleShow}>
+                    Delete
+                </Button>
+
+                <Modal
+                    show={this.state.show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Confirm Delete Entry</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Are you sure you want to delete the record for {artist.name}?
+                        Please enter your email to confirm deletion of this entry:
+                        <div>
+                            <label className="form-label">Email: </label>
+                            <input type="text" className="form-control" name="email" placeholder="email" value={this.state.email} onChange={this.updateFormField} />
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Cancel
+                        </Button>
+                        <Button variant="primary" onClick={() => {this.processDelete(artist._id)}}>Confirm delete</Button>
+                    </Modal.Footer>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+
     render() {
-        return(
+        return (
             <React.Fragment>
                 {this.state.data.map(e => (
-                    <React.Fragment>
-                        <div className="card">
-                        <div className="card-title">{e.name}</div>
-                        <div className="card-body">{e.gender}</div>
+                    <React.Fragment key={e._id}>
+                        <div className="card" style={{ "width": "18rem" }}>
+                            <img src="..." class="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{e.name}</h5>
+                                <p className="card-text">
+                                    Gender: {e.gender}<br />
+                                    Years of experience: {e.yearsOfExperience}<br />
+                                    Apprentice? {e.apprentice}<br />
+                                    Methods: {e.method.map(a => (
+                                        <span class="badge rounded-pill bg-secondary" key={a}>{a}</span>
+                                    ))}<br />
+                                    Temporary? {e.temporary}<br />
+                                    {/* Style: {e.style.map (a => (
+                                        <span class="badge rounded-pill bg-secondary">{a}</span>
+                                    ))}<br/> */}
+                                    Ink: {e.ink.map(a => (
+                                        <span class="badge rounded-pill bg-secondary" key={a}>{a}</span>
+                                    ))}<br />
+
+
+                                </p>
+                                <a href="#" class="btn btn-primary">View</a>
+                                {this.ConfirmDelete(e)}
+                            </div>
                         </div>
                     </React.Fragment>
                 ))}
