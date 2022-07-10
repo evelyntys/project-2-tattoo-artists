e9292e1e49852258a24ff37e722b2fae51b034a8
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-10 11:19:31 +0000

    added components to render from navbar

---
+++ tattoo-artists/src/Explore.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Explore extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>Explore artists: </h1>
+            </React.Fragment>
+        )
+    }
+}
