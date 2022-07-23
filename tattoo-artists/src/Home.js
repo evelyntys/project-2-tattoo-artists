34e5b3870c2e9bddd5dbca126dc68670e72f8912
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-23 09:41:18 +0000

    updated images and styling for nav buttons

--- tattoo-artists/src/Home.js
+++ tattoo-artists/src/Home.js
@@ -4,25 +4,37 @@ export default class Home extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div style={{ "maxHeight": "300px", "width": "100vw", "position": "relative" }}>
-          <img src="https://drive.google.com/uc?id=12PQFEtg6EJ7kSUiO8MWeHwYQ0O74b8hd" style={{ "objectFit": "cover", 'width': "100%", 'maxHeight': '300px' }} alt="gif of tattoo process" />
-          {/* <div style={{ "color": "white", "backgroundColor": "black", 'width': "100%", "position": "absolute", "bottom": "0"}}>
+                <div className="header-banner">
+                    <img className="header-image" src="https://drive.google.com/uc?id=12PQFEtg6EJ7kSUiO8MWeHwYQ0O74b8hd" alt="female artist tattooing a client" />
+                    <div className="overlay d-flex align-items-center justify-content-center">
+                        <h1>TATTOOFINDWHO</h1>
+                    </div>
+                </div>
+                {/* <div style={{ "maxHeight": "300px", "width": "100vw", "position": "relative" }}>
+          <img src="https://drive.google.com/uc?id=12PQFEtg6EJ7kSUiO8MWeHwYQ0O74b8hd" style={{ "objectFit": "cover", 'width': "100%", 'maxHeight': '300px' }} alt="gif of tattoo process" /> */}
+                {/* <div style={{ "color": "white", "backgroundColor": "black", 'width': "100%", "position": "absolute", "bottom": "0"}}>
           TATTOOFINDWHO
           </div> */}
-        </div>
+                {/* </div> */}
                 <div className="container">
                     Interested in getting a tattoo?
                     <div className="row">
-                        <div className="container col-12 col-md-4 explore" style={{ "border": "1px solid black", "height": "300px" }}>
-                            <div className='nav-btn d-flex justify-content-center align-items-center' onClick={() => { this.props.ChangePages('explore') }}>Explore</div>
+                        <div className="container p-0 col-12 col-md-4 faq" style={{ "border": "1px solid black", "height": "300px" }}>
+                            <div className='nav-btn d-flex justify-content-center align-items-center'>
+                                <h1 onClick={() => { this.props.ChangePages('faq') }}>FAQ</h1>
+                            </div>
                         </div>
 
-                        <div className="container col-12 col-md-4 faq" style={{ "border": "1px solid black", "height": "300px" }}>
-                            <div className='nav-btn d-flex justify-content-center align-items-center' onClick={() => { this.props.ChangePages('faq') }}>FAQ</div>
+                        <div className="container p-0 col-12 col-md-4 explore" style={{ "border": "1px solid black", "height": "300px" }}>
+                            <div className='nav-btn d-flex justify-content-center align-items-center'>
+                                <h1 onClick={() => { this.props.ChangePages('explore') }}>Explore</h1>
+                            </div>
                         </div>
 
-                        <div className="container col-12 col-md-4 add-new" style={{ "border": "1px solid black", "height": "300px" }}>
-                            <div className='nav-btn d-flex justify-content-center align-items-center' onClick={() => { this.props.ChangePages('add-new') }}>Add new artist</div>
+                        <div className="container p-0 col-12 col-md-4 add-new" style={{ "border": "1px solid black", "height": "300px" }}>
+                            <div className='nav-btn d-flex justify-content-center align-items-center'>
+                                <h1 onClick={() => { this.props.ChangePages('add-new') }}>Add new artist</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
