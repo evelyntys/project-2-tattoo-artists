2034e5d98c73584a418295c1c9f53d7210b2e543
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-26 06:53:09 +0000

    updated home page styling

--- tattoo-artists/src/pages/Home.js
+++ tattoo-artists/src/pages/Home.js
@@ -7,32 +7,27 @@ export default class Home extends React.Component {
                 <div className="header-banner mb-2">
                     <img className="header-image" src="https://drive.google.com/uc?id=12PQFEtg6EJ7kSUiO8MWeHwYQ0O74b8hd" alt="tattoo artist tattooing a client" />
                     <div className="overlay d-flex align-items-center justify-content-center">
-                    <img className="header-logo" src={require("../images/tattoo.png")} style={{"height": "70px"}}/> <h1> TATTOOFINDWHO</h1>
+                    <img className="header-logo" src={require("../images/tattoo.png")} style={{"height": "70px"}} alt="logo"/> <h1> TATTOOFINDWHO</h1>
                     </div>
                 </div>
-                {/* <div style={{ "maxHeight": "300px", "width": "100vw", "position": "relative" }}>
-          <img src="https://drive.google.com/uc?id=12PQFEtg6EJ7kSUiO8MWeHwYQ0O74b8hd" style={{ "objectFit": "cover", 'width': "100%", 'maxHeight': '300px' }} alt="gif of tattoo process" /> */}
-                {/* <div style={{ "color": "white", "backgroundColor": "black", 'width': "100%", "position": "absolute", "bottom": "0"}}>
-          TATTOOFINDWHO
-          </div> */}
-                {/* </div> */}
+
                 <div className="container">
                     <div className="row">
-                        <div className="container p-0 col-12 col-md-4 faq">
-                            <div className='nav-btn d-flex justify-content-center align-items-center'>
-                                <h1 className="home-nav" onClick={() => { this.props.ChangePages('faq') }}>FAQs</h1>
+                        <div className="container p-0 col-12 my-1 faq">
+                            <div className='nav-btn d-flex justify-content-center align-items-center' onClick={() => { this.props.ChangePages('faq') }}>
+                                <h1 className="home-nav">FAQs</h1>
                             </div>
                         </div>
 
-                        <div className="container p-0 col-12 col-md-4 explore">
-                            <div className='nav-btn d-flex justify-content-center align-items-center'>
-                                <h1 className="home-nav" onClick={() => { this.props.ChangePages('explore') }}>Explore</h1>
+                        <div className="container p-0 col-12 my-1 explore">
+                            <div className='nav-btn d-flex justify-content-center align-items-center' onClick={() => { this.props.ChangePages('explore') }}>
+                                <h1 className="home-nav">Explore</h1>
                             </div>
                         </div>
 
-                        <div className="container p-0 col-12 col-md-4 add-new">
-                            <div className='nav-btn d-flex justify-content-center align-items-center'>
-                                <h1 className="home-nav" onClick={() => { this.props.ChangePages('add-new') }}>Add new artist</h1>
+                        <div className="container p-0 col-12 my-1 add-new">
+                            <div className='nav-btn d-flex justify-content-center align-items-center' onClick={() => { this.props.ChangePages('add-new') }}>
+                                <h1 className="home-nav">Add new artist</h1>
                             </div>
                         </div>
                     </div>
